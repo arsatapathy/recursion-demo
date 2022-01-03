ec522d14a92a609157d662a6522d05e38d790ed3
arsatapathy <ashish.satapathy2014@gmail.com>
2022-01-03 12:37:23 +0530

    Palindrom

--- src/main/java/com/arsatapathy/Palindrome.java
+++ src/main/java/com/arsatapathy/Palindrome.java
@@ -1,2 +1,18 @@
-package com.arsatapathy;public class Palimdrome {
+package com.arsatapathy;
+
+public class Palindrome {
+
+    public static boolean isPalindrome(String input) {
+
+        if (input.isEmpty() || input.length() == 1) {
+            return true;
+        }
+
+        if (input.charAt(0) == input.charAt(input.length() - 1)) {
+            return isPalindrome(input.substring(1, input.length() - 1));
+        }
+        else {
+            return false;
+        }
+    }
 }
--- src/test/java/com/arsatapathy/PalindromeTest.java
+++ src/test/java/com/arsatapathy/PalindromeTest.java
@@ -1,2 +1,18 @@
-package com.arsatapathy;public class PalindromeTest {
+package com.arsatapathy;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class PalindromeTest {
+
+    @Test
+    public void test_01() {
+        Assertions.assertTrue(Palindrome.isPalindrome("racecar"));
+    }
+
+
+    @Test
+    public void test_02() {
+        Assertions.assertFalse(Palindrome.isPalindrome("carrace"));
+    }
 }
